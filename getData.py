a112d7ecab87f2ff0aed6826c6d6fa1a9a591649
mrubin02 <maddierubin02@gmail.com>
2023-10-29 18:04:43 -0400

    updated dividend_data

--- getData.py
+++ getData.py
@@ -3,7 +3,7 @@ import datetime
 import pandas as pd
 
 # Set your App Key
-ek.set_app_key('YOUR_APP_KEY')
+ek.set_app_key('7f3b79b0b2a44576a25f3d6a234cfefc81e7fbb5')
 
 # Get the date 5 years ago from today
 end_date = datetime.date.today()
@@ -17,14 +17,33 @@ aristocrats = [
 # Create an empty dataframe to store the results
 all_data = pd.DataFrame()
 
-# Fetch historical data for each code
-for ric in aristocrats:
-    data, err = ek.get_data(ric, 
-                            ["DATE", "CLOSE"],
-                            {'start_date': start_date, 'end_date': end_date, 'frequency': 'daily'})
-    
-    data['RIC'] = ric  # Add a column to differentiate the data by stock
-    all_data = all_data.append(data, ignore_index=True)
+data, data_err = ek.get_data(instruments = aristocrats, 
+                           fields = [
+                            'TR.OPENPRICE(Adjusted=0)',
+                            'TR.HIGHPRICE(Adjusted=0)',
+                            'TR.LOWPRICE(Adjusted=0)',
+                            'TR.CLOSEPRICE(Adjusted=0)',
+                            'TR.PriceCloseDate'
+                            ],
+                            parameters = {
+                                'SDate': start_date.strftime("%Y-%m-%d"),
+                                'EDate': end_date.strftime("%Y-%m-%d"),
+                                'Frq': 'D'
+                            })
 
-# Save the data to a CSV
-all_data.to_csv('dividend_aristocrats_history.csv', index=False)
+
+##### prices
+data.rename(
+    columns = {
+        'Open Price':'open',
+        'High Price':'high',
+        'Low Price':'low',
+        'Close Price':'close'
+    },
+    inplace = True
+)
+data.dropna(inplace=True)
+data['Date'] = pd.to_datetime(data['Date']).dt.date
+# save as csv so you can open in Excel if you want
+data.to_csv('dividend_data.csv', index=False)
+
