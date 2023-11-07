10eb7f3bbb0e0692f850461fdab38f68ec2111d8
mrubin02 <maddierubin02@gmail.com>
2023-11-07 16:17:16 -0500

    added get_features.py

---
+++ get_features.py
@@ -0,0 +1,24 @@
+import eikon as ek
+import datetime
+import pandas as pd
+
+# Set your App Key
+ek.set_app_key('7f3b79b0b2a44576a25f3d6a234cfefc81e7fbb5')
+
+# Get the date 5 years ago from today
+end_date = datetime.date.today()
+start_date = end_date - datetime.timedelta(days=5*365)
+
+# List of symbols for dividend aristocrats (example list, might be different based on your data source)
+features = ['XLV.PH', '.TRGSPI', '.TRGSPS']
+
+# Create an empty dataframe to store the results
+all_data = pd.DataFrame()
+
+data  = ek.get_timeseries(['XLV.PH', '.TRGSPI', '.TRGSPS'],
+                            fields = ['CLOSE'],
+                            start_date = start_date.strftime("%Y-%m-%d"),
+                            end_date = end_date.strftime("%Y-%m-%d"))
+
+data.dropna(inplace=True)
+data.to_csv('feature_data.csv', index=True)
