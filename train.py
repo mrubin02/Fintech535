69fdc036585303441dd78ef15b21c64a778a6d99
mrubin02 <maddierubin02@gmail.com>
2023-11-14 15:00:18 -0500

    update linear predictions

--- train.py
+++ train.py
@@ -1,13 +1,59 @@
 import pandas as pd 
 import numpy as np 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import accuracy_score, roc_curve, roc_auc_score, RocCurveDisplay
+import matplotlib.pyplot as plt
 
+# read weights and set date to index
 weights = pd.read_csv('optimized_portfolio_weights_daily.csv')
 weights.columns.values[0] = 'date'
 weights.set_index('date', inplace = True)
 
-weights[weights<.01] = 0
-weights[weights>=.01] = 1
+# set threshold to be included in portfolio 
+threshold = .000000001 
 
-print(weights)
-
+# mask weights
+weights[weights<threshold] = 0
+weights[weights>=threshold] = 1
 weights.to_csv('weights_labels.csv')
+
+# get features
+features = pd.read_csv('feature_data.csv')
+features.set_index('Date', inplace = True)
+merged = pd.merge(left = weights, right = features, left_index = True,right_index = True)
+merged.to_csv("features_and_labels.csv")
+
+X = merged[['XLV.PH', '.TRGSPI','.TRGSPS']]
+y = merged.iloc[:,:65]
+
+print(X)
+print(y)
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, shuffle = True)
+
+"""model = LinearRegression()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_valid)
+acc = accuracy_score(y_pred, y_valid)
+print(acc)"""
+
+for i in y_train:
+    y = y_train[i].values.reshape(-1,1)
+    model = LinearRegression()
+    model.fit(X_train, y)
+    y_pred = model.predict(X_valid)
+    y_pred[y_pred < 0.2] = 0
+    y_pred[y_pred >= 0.2] = 1
+    acc = accuracy_score(y_pred, y_valid[i])
+    fpr, tpr, thresholds = roc_curve(y_valid[i], y_pred)
+    auc = roc_auc_score(y_valid[i], y_pred)
+    if auc>0.5:
+        print(i + " auc score: " + str(auc))
+    RocCurveDisplay.from_predictions(y_valid[i], y_pred)
+
+
+
+
+    
+
+
