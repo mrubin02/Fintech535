c7230dfe9c8bc0e887285ea62fbb5a9847620660
Samuel Seelig <sjs100@duke.edu>
2023-11-29 17:02:06 -0500

    train on previous day

--- get_features.py
+++ get_features.py
@@ -3,19 +3,20 @@ import datetime
 import pandas as pd
 
 # Set your App Key
-ek.set_app_key('7f3b79b0b2a44576a25f3d6a234cfefc81e7fbb5')
+#ek.set_app_key('7f3b79b0b2a44576a25f3d6a234cfefc81e7fbb5') #maddie app key
+ek.set_app_key('9190767d89924e47b6125365150557616654eb0d') #sam app key
 
 # Get the date 5 years ago from today
 end_date = datetime.date.today()
 start_date = end_date - datetime.timedelta(days=5*365)
 
 # List of symbols for dividend aristocrats (example list, might be different based on your data source)
-features = ['XLV.PH', '.TRGSPI', '.TRGSPS', 'VNQ', 'SDY', 'XLU','SPLV.K']
+features = ['XLV.PH', '.TRGSPI', '.TRGSPS', 'VNQ', 'SDY', 'XLU','SPLV.K', 'XLI', 'XLP']
 
 # Create an empty dataframe to store the results
 all_data = pd.DataFrame()
 
-data  = ek.get_timeseries(['XLV.PH', '.TRGSPI', '.TRGSPS', 'VNQ', 'SDY', 'XLU','SPLV.K'],
+data  = ek.get_timeseries([['XLV.PH', '.TRGSPI', '.TRGSPS', 'VNQ', 'SDY', 'XLU','SPLV.K', 'XLI', 'XLP']],
                             fields = ['CLOSE'],
                             start_date = start_date.strftime("%Y-%m-%d"),
                             end_date = end_date.strftime("%Y-%m-%d"))
--- train.py
+++ train.py
@@ -1,38 +1,30 @@
 import pandas as pd 
 import numpy as np 
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import accuracy_score, roc_curve, roc_auc_score, RocCurveDisplay
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, roc_curve, roc_auc_score, RocCurveDisplay, precision_score, recall_score, f1_score
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 
-# read weights and set date to index
-weights = pd.read_csv('optimized_portfolio_weights_daily.csv')
-weights.columns.values[0] = 'date'
-weights.set_index('date', inplace = True)
-
-# set threshold to be included in portfolio 
-threshold = .000001 
-
-# mask weights
-weights[weights<threshold] = 0
-weights[weights>=threshold] = 1
-weights.to_csv('weights_labels.csv')
-
-# get features
-features = pd.read_csv('feature_data.csv')
-features.set_index('Date', inplace = True)
-merged = pd.merge(left = weights, right = features, left_index = True,right_index = True)
-merged.to_csv("features_and_labels.csv")
+# Your code for reading data and preprocessing...
 
 X = merged[['XLV.PH', '.TRGSPI', '.TRGSPS', 'VNQ', 'SDY', 'XLU', 'SPLV.K']]
-scaler = StandardScaler()
-scaler.fit_transform(X)
-
 y = merged.iloc[:,:65]
 
-X_train, X_valid, y_train, y_valid = train_test_split(X, y, shuffle = True)
+# Shift y upwards by one row
+y = y.shift(-1)
 
+# Drop the last row from X and the first row from y
+X = X.iloc[:-1, :]
+y = y.iloc[:-1, :]
+
+# Continue with scaling and model training
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+X_train, X_valid, y_train, y_valid = train_test_split(X_scaled, y, shuffle = True)
+
+# Your model training and evaluation code...
 """model = LinearRegression()
 model.fit(X_train, y_train)
 y_pred = model.predict(X_valid)
