e14a119fc79959bd01f85e155c511e38dca8e0c7
plutothespacedog <45442951+plutothespacedog@users.noreply.github.com>
2023-12-12 18:01:09 -0500

    time series

---
+++ trainNoFuture.py
@@ -0,0 +1,74 @@
+import pandas as pd 
+import numpy as np 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import accuracy_score, roc_curve, roc_auc_score, RocCurveDisplay
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+
+# read weights and set date to index
+weights = pd.read_csv('optimized_portfolio_weights_daily.csv')
+weights.columns.values[0] = 'date'
+weights.set_index('date', inplace = True)
+
+# set threshold to be included in portfolio 
+threshold = .000001 
+
+# mask weights
+weights[weights<threshold] = 0
+weights[weights>=threshold] = 1
+weights.to_csv('weights_labels.csv')
+
+# get features
+features = pd.read_csv('feature_data.csv')
+features.set_index('Date', inplace = True)
+merged = pd.merge(left = weights, right = features, left_index = True,right_index = True)
+merged.to_csv("features_and_labels.csv")
+
+X = merged[['XLV.PH', '.TRGSPI', '.TRGSPS', 'VNQ', 'SDY', 'XLU','SPLV.K','XLI', 'XLP', '.BCOMCLC', 'SLX', '.DRG', '.MIWO0CS00PUS', 'GE', "BA"]]
+y = merged.iloc[:,:65]
+
+# Shift y upwards by one row
+y = y.shift(-1)
+
+# Drop the last row from X and the first row from y
+X = X.iloc[:-1, :]
+y = y.iloc[:-1, :]
+
+# Continue with scaling and model training
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+# Calculate the index to split the data on
+split_index = int(len(X_scaled) * 0.8)
+
+# Split the data without shuffling, ensuring that the model is trained on historical data only
+X_train, X_valid = X_scaled[:split_index], X_scaled[split_index:]
+y_train, y_valid = y[:split_index], y[split_index:]
+
+"""model = LinearRegression()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_valid)
+acc = accuracy_score(y_pred, y_valid)
+print(acc)"""
+
+aucs = []
+for i in y_train:
+    y = y_train[i].values.reshape(-1,1)
+    model = LinearRegression()
+    model.fit(X_train, y)
+    y_pred = model.predict(X_valid)
+    y_pred[y_pred < 0.1] = 0
+    y_pred[y_pred >= 0.1] = 1
+    try: 
+        acc = accuracy_score(y_pred, y_valid[i])
+        fpr, tpr, thresholds = roc_curve(y_valid[i], y_pred)
+        auc = roc_auc_score(y_valid[i], y_pred)
+        aucs += [auc]
+        if auc>0.5:
+            print(i + " auc score: " + str(auc))
+        RocCurveDisplay.from_predictions(y_valid[i], y_pred)
+    except: 
+        print('guessed all zeroes')
+
+print(np.average(aucs))
